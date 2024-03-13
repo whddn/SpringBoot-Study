4701335dbd941f27c926bfc13183cfe83120cb39
이종우 <ghjkl133120@naver.com>
2024-03-13 16:30:35 +0900

    docs: 템플릿 수정하기

--- sbb/src/main/java/com/mysite/sbb/question/QuestionController.java
+++ sbb/src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -31,6 +31,11 @@ public class QuestionController {
         model.addAttribute("question", question);
         return "question_detail";
     }
+    @GetMapping("/create")
+    public String questionCreate(QuestionForm questionForm) {
+        return "question_form";
+    }
+
     @PostMapping("/create")
     public String questionCreate(@Valid QuestionForm questionForm, BindingResult bindingResult) {
         if (bindingResult.hasErrors()) {
