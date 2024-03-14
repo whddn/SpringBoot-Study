f300a85d28aeb58d5c43491dd96eb4b36b35e196
이종우 <ghjkl133120@naver.com>
2024-03-14 16:37:28 +0900

    docs: 중복 회원 가입 방지

--- sbb/src/main/java/com/mysite/sbb/user/UserController.java
+++ sbb/src/main/java/com/mysite/sbb/user/UserController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import lombok.RequiredArgsConstructor;
+import lombok.RequiredArgsConstructor;import org.springframework.dao.DataIntegrityViolationException;
 
 @RequiredArgsConstructor
 @Controller
@@ -33,10 +33,18 @@ public class UserController {
                     "2개의 패스워드가 일치하지 않습니다.");
             return "signup_form";
         }
-
-        userService.create(userCreateForm.getUsername(),
-                userCreateForm.getEmail(), userCreateForm.getPassword1());
-
+        try {
+            userService.create(userCreateForm.getUsername(),
+                    userCreateForm.getEmail(), userCreateForm.getPassword1());
+        }catch(DataIntegrityViolationException e) {
+            e.printStackTrace();
+            bindingResult.reject("signupFailed", "이미 등록된 사용자입니다.");
+            return "signup_form";
+        }catch(Exception e) {
+            e.printStackTrace();
+            bindingResult.reject("signupFailed", e.getMessage());
+            return "signup_form";
+        }
         return "redirect:/";
     }
 }
