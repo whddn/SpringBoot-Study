234a20a84a41a55fd01f330dee7e284e9144be9f
이종우 <ghjkl133120@naver.com>
2024-03-14 11:08:24 +0900

    docs: 최신순으로 데이터 조회

--- sbb/src/main/java/com/mysite/sbb/question/QuestionService.java
+++ sbb/src/main/java/com/mysite/sbb/question/QuestionService.java
@@ -8,7 +8,9 @@ import java.time.LocalDateTime;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Pageable;import java.util.ArrayList;
+import java.util.List;
+import org.springframework.data.domain.Sort;
 
 @RequiredArgsConstructor
 @Service
@@ -29,10 +31,12 @@ public class QuestionService {
     }
 
     public Page<Question> getList(int page) {
-        Pageable pageable = PageRequest.of(page, 10);
+        List<Sort.Order> sorts = new ArrayList<>();
+        sorts.add(Sort.Order.desc("createDate"));
+        Pageable pageable = PageRequest.of(page, 10, Sort.by(sorts));
         return this.questionRepository.findAll(pageable);
     }
-    
+
     public void create(String subject, String content) {
         Question q = new Question();
         q.setSubject(subject);
