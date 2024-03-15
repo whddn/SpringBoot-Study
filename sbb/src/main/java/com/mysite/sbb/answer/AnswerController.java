648c645c2e00caf509d8a195fb9d65b7aa6bf628
이종우 <ghjkl133120@naver.com>
2024-03-15 15:22:11 +0900

    docs: 답변 컨트롤러와 서비스 업데이트

--- sbb/src/main/java/com/mysite/sbb/answer/AnswerController.java
+++ sbb/src/main/java/com/mysite/sbb/answer/AnswerController.java
@@ -11,6 +11,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import jakarta.validation.Valid;
 import org.springframework.validation.BindingResult;
+import com.mysite.sbb.user.SiteUser;
+import com.mysite.sbb.user.UserService;
+import java.security.Principal;
 
 @RequestMapping("/answer")
 @RequiredArgsConstructor
@@ -19,16 +22,18 @@ public class AnswerController {
 
     private final QuestionService questionService;
     private final AnswerService answerService;
+    private final UserService userService;
 
     @PostMapping("/create/{id}")
     public String createAnswer(Model model, @PathVariable("id") Integer id,
-                               @Valid AnswerForm answerForm, BindingResult bindingResult){
+                               @Valid AnswerForm answerForm, BindingResult bindingResult, Principal principal){
         Question question = this.questionService.getQuestion(id);
+        SiteUser siteUser = this.userService.getUser(principal.getName());
         if (bindingResult.hasErrors()) {
             model.addAttribute("question", question);
             return "question_detail";
         }
-        this.answerService.create(question, answerForm.getContent());
+        this.answerService.create(question, answerForm.getContent(), siteUser);
         return String.format("redirect:/question/detail/%s", id);
     }
 }
--- sbb/src/main/java/com/mysite/sbb/user/UserService.java
+++ sbb/src/main/java/com/mysite/sbb/user/UserService.java
@@ -3,7 +3,8 @@ package com.mysite.sbb.user;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import lombok.RequiredArgsConstructor;
+import lombok.RequiredArgsConstructor;import java.util.Optional;
+import com.mysite.sbb.DataNotFoundException;
 
 @RequiredArgsConstructor
 @Service
@@ -20,4 +21,13 @@ public class UserService {
         this.userRepository.save(user);
         return user;
     }
+    public SiteUser getUser(String username) {
+        Optional<SiteUser> siteUser = this.userRepository.findByusername(username);
+        if (siteUser.isPresent()) {
+            return siteUser.get();
+        } else {
+            throw new DataNotFoundException("siteuser not found");
+        }
+    }
+    
 }
