bc49b8b9e4089b302c4fcfe194216208602571eb
이종우 <ghjkl133120@naver.com>
2024-03-18 14:36:49 +0900

    docs: 질문 서비스와 컨트롤러 수정

--- sbb/src/main/java/com/mysite/sbb/question/QuestionService.java
+++ sbb/src/main/java/com/mysite/sbb/question/QuestionService.java
@@ -52,4 +52,8 @@ public class QuestionService {
         question.setModifyDate(LocalDateTime.now());
         this.questionRepository.save(question);
     }
+    public void delete(Question question) {
+        this.questionRepository.delete(question);
+    }
+
 }
