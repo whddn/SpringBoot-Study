07467191e855d6c0cdbee7346e2f300a31643e84
이종우 <ghjkl133120@naver.com>
2024-03-18 14:59:43 +0900

    docs: POST방식의 답변 수정

--- sbb/src/main/java/com/mysite/sbb/answer/AnswerController.java
+++ sbb/src/main/java/com/mysite/sbb/answer/AnswerController.java
@@ -50,5 +50,19 @@ public class AnswerController {
         answerForm.setContent(answer.getContent());
         return "answer_form";
     }
-    
+    @PreAuthorize("isAuthenticated()")
+    @PostMapping("/modify/{id}")
+    public String answerModify(@Valid AnswerForm answerForm, BindingResult bindingResult,
+                               @PathVariable("id") Integer id, Principal principal) {
+        if (bindingResult.hasErrors()) {
+            return "answer_form";
+        }
+        Answer answer = this.answerService.getAnswer(id);
+        if (!answer.getAuthor().getUsername().equals(principal.getName())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "수정권한이 없습니다.");
+        }
+        this.answerService.modify(answer, answerForm.getContent());
+        return String.format("redirect:/question/detail/%s", answer.getQuestion().getId());
+    }
+
 }
