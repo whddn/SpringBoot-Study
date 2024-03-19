2117375c3f82da0997235a41b29517a4b0a2fedd
이종우 <ghjkl133120@naver.com>
2024-03-19 13:37:14 +0900

    docs: Query애너테이션 사용

--- sbb/src/main/java/com/mysite/sbb/question/QuestionService.java
+++ sbb/src/main/java/com/mysite/sbb/question/QuestionService.java
@@ -60,7 +60,8 @@ public class QuestionService {
         sorts.add(Sort.Order.desc("createDate"));
         Pageable pageable = PageRequest.of(page, 10, Sort.by(sorts));
         Specification<Question> spec = search(kw);
-        return this.questionRepository.findAll(spec, pageable);
+        return this.questionRepository.findAllByKeyword(kw, pageable);
+
     }
 
     public void create(String subject, String content,  SiteUser user) {
